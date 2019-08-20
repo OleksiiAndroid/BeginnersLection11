cb9fc4c82878073ecdfc7ade547c8c4583866649
Oleksii Ponomarov <spinne@ua.fm>
2019-08-20 12:04:38 +0300

    update api 29

--- app/src/main/java/ua/com/webacademy/beginnerslection11/MainActivity.java
+++ app/src/main/java/ua/com/webacademy/beginnerslection11/MainActivity.java
@@ -1,32 +1,23 @@
 package ua.com.webacademy.beginnerslection11;
 
-import android.Manifest;
-import android.content.Context;
+import android.app.ProgressDialog;
+import android.content.ComponentName;
 import android.content.Intent;
-import android.content.SharedPreferences;
-import android.content.pm.PackageManager;
-import android.os.Build;
+import android.content.ServiceConnection;
+import android.os.AsyncTask;
 import android.os.Bundle;
-import android.os.Environment;
-import android.preference.PreferenceManager;
-import android.support.v4.content.ContextCompat;
-import android.support.v7.app.AppCompatActivity;
+import android.os.IBinder;
 import android.view.View;
 import android.widget.Toast;
 
-import com.google.gson.Gson;
-import com.google.gson.GsonBuilder;
-
-import java.io.BufferedReader;
-import java.io.BufferedWriter;
-import java.io.File;
-import java.io.FileOutputStream;
-import java.io.FileReader;
-import java.io.InputStream;
-import java.io.InputStreamReader;
-import java.io.OutputStreamWriter;
+import androidx.appcompat.app.AppCompatActivity;
 
 public class MainActivity extends AppCompatActivity {
+
+    private ProgressDialog mDialog;
+
+    private SaveTask mSaveTask;
+    private GetTask mGetTask;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -34,194 +25,194 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
     }
 
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+
+        if (mSaveTask != null) {
+            mSaveTask.cancel(true);
+        }
+        if (mGetTask != null) {
+            mGetTask.cancel(true);
+        }
+    }
+
     public void OnClick(View v) {
-        SharedPreferences preferences;
-        SharedPreferences.Editor editor;
+        final Student student;
 
         switch (v.getId()) {
             case R.id.button:
-                preferences = getPreferences(MODE_PRIVATE);
-                editor = preferences.edit();
+                mDialog = new ProgressDialog(this);
+                mDialog.setMessage("Wait...");
+                mDialog.setCancelable(false);
+                mDialog.show();
 
-                editor.putString("Test", "test");
-
-                editor.commit();
+                student = new Student("Ivan", "Ivanov", 22);
+                StudentIntentService.insertStudent(this, student);
                 break;
             case R.id.button2:
-                preferences = getPreferences(MODE_PRIVATE);
-                String text1 = preferences.getString("Test", "");
+                mDialog = new ProgressDialog(this);
+                mDialog.setMessage("Wait...");
+                mDialog.setCancelable(false);
+                mDialog.show();
 
-                Toast.makeText(this, text1, Toast.LENGTH_SHORT).show();
+                StudentIntentService.getStudent(this, 1);
                 break;
-            case R.id.button3:
-                preferences = getSharedPreferences("MyPref", MODE_PRIVATE);
-                editor = preferences.edit();
+            case R.id.button3: {
+                ServiceConnection connection = new ServiceConnection() {
+                    @Override
+                    public void onServiceConnected(ComponentName componentName, IBinder iBinder) {
+                        StudentService studentService = ((StudentService.ServiceBinder) iBinder).getService();
 
-                editor.putString("Test", "test2");
+                        long id = studentService.saveStudent(new Student("Ivan", "Ivanov", 22));
+                        Toast.makeText(MainActivity.this, "id:" + id, Toast.LENGTH_SHORT).show();
+                    }
 
-                editor.commit();
-                break;
-            case R.id.button4:
-                preferences = getSharedPreferences("MyPref", MODE_PRIVATE);
-                String text3 = preferences.getString("Test", "");
+                    @Override
+                    public void onServiceDisconnected(ComponentName componentName) {
 
-                Toast.makeText(this, text3, Toast.LENGTH_SHORT).show();
-                break;
+                    }
+                };
+
+                Intent intent = new Intent(this, StudentService.class);
+                bindService(intent, connection, BIND_AUTO_CREATE);
+            }
+            break;
+            case R.id.button4: {
+                ServiceConnection connection = new ServiceConnection() {
+                    @Override
+                    public void onServiceConnected(ComponentName componentName, IBinder iBinder) {
+                        StudentService studentService = ((StudentService.ServiceBinder) iBinder).getService();
+
+                        Student student = studentService.getStudent(1);
+                        Toast.makeText(MainActivity.this, student.FirstName, Toast.LENGTH_SHORT).show();
+                    }
+
+                    @Override
+                    public void onServiceDisconnected(ComponentName componentName) {
+
+                    }
+                };
+
+                Intent intent = new Intent(this, StudentService.class);
+                bindService(intent, connection, BIND_AUTO_CREATE);
+            }
             case R.id.button5:
-                Intent intent = new Intent(this, PreferenceActivity.class);
-                startActivity(intent);
-                break;
-            case R.id.button7:
-                preferences = PreferenceManager.getDefaultSharedPreferences(this);
+                student = new Student("Ivan", "Ivanov", 22);
 
-                String text4 = preferences.getString("pref2", "");
-                Toast.makeText(this, text4, Toast.LENGTH_SHORT).show();
+                mSaveTask = new SaveTask();
+                mSaveTask.execute(student);
                 break;
-            case R.id.button8:
-                saveInternalFile("MyFile1.txt", "File data");
-                break;
-            case R.id.button9:
-                String text9 = readInternalFile("MyFile1.txt");
-                Toast.makeText(this, text9, Toast.LENGTH_SHORT).show();
-                break;
-            case R.id.button10:
-                if (hasPermission(Manifest.permission.WRITE_EXTERNAL_STORAGE)) {
-                    if (Environment.getExternalStorageState().equals(Environment.MEDIA_MOUNTED)) {
-                        File folder = Environment.getExternalStorageDirectory();
-                        folder = new File(folder.getAbsolutePath() + "/MyFolder");
-
-                        saveExternalFile(folder, "MyFile2.txt", "File data");
-                    }
-                } else {
-                    Toast.makeText(this, "You don't have permission for this", Toast.LENGTH_SHORT).show();
-                }
-                break;
-            case R.id.button11:
-                if (hasPermission(Manifest.permission.WRITE_EXTERNAL_STORAGE)) {
-                    if (Environment.getExternalStorageState().equals(Environment.MEDIA_MOUNTED)) {
-                        File folder = Environment.getExternalStorageDirectory();
-                        folder = new File(folder.getAbsolutePath() + "/MyFolder");
-
-                        if (folder.exists()) {
-                            String text11 = readExternalFile(folder, "MyFile2.txt");
-                            Toast.makeText(this, text11, Toast.LENGTH_SHORT).show();
-                        }
-                    }
-                } else {
-                    Toast.makeText(this, "You don't have permission for this", Toast.LENGTH_SHORT).show();
-                }
-                break;
-            case R.id.button12:
-                Student student = new Student("Ivan", "Ivanov", 22);
-
-                Gson gson = new GsonBuilder().create();
-                String json = gson.toJson(student);
-
-                saveInternalFile("Student.txt", json);
-                break;
-            case R.id.button13:
-                String json2 = readInternalFile("Student.txt");
-
-                Gson gson2 = new GsonBuilder().create();
-                Student student2 = gson2.fromJson(json2, Student.class);
-
-                Toast.makeText(this, student2.FirstName, Toast.LENGTH_SHORT).show();
-                break;
-            case R.id.button14:
-                if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
-                    String[] permissions = {
-                            Manifest.permission.WRITE_EXTERNAL_STORAGE,
-                            Manifest.permission.READ_EXTERNAL_STORAGE
-                    };
-
-                    requestPermissions(permissions, 0);
-                } else {
-                    Toast.makeText(this, "You can request permissions only in Android 6+", Toast.LENGTH_SHORT).show();
-                }
+            case R.id.button6:
+                mGetTask = new GetTask();
+                mGetTask.execute(1l);
                 break;
         }
     }
 
-    private boolean hasPermission(String permission) {
-        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
-            return ContextCompat.checkSelfPermission(this, permission) == PackageManager.PERMISSION_GRANTED;
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+
+        if (resultCode == RESULT_OK) {
+            if (requestCode == StudentIntentService.REQUEST_CODE_SAVE_STUDENT) {
+                long id = data.getLongExtra(StudentIntentService.EXTRA_ID, 0);
+                Toast.makeText(this, "id:" + id, Toast.LENGTH_SHORT).show();
+            } else if (requestCode == StudentIntentService.REQUEST_CODE_GET_STUDENT) {
+                Student student = data.getParcelableExtra(StudentIntentService.EXTRA_STUDENT);
+                Toast.makeText(this, student.FirstName, Toast.LENGTH_SHORT).show();
+            }
         }
 
-        return true;
-    }
-
-    private void saveInternalFile(String fileName, String data) {
-        try {
-            BufferedWriter writer = new BufferedWriter(new OutputStreamWriter(openFileOutput(fileName, Context.MODE_PRIVATE)));
-
-            writer.write(data);
-            writer.flush();
-            writer.close();
-        } catch (Exception e) {
-            e.printStackTrace();
+        if (mDialog != null && mDialog.isShowing()) {
+            mDialog.dismiss();
         }
     }
 
-    private String readInternalFile(String fileName) {
-        try {
-            StringBuilder builder = new StringBuilder();
 
-            BufferedReader reader = new BufferedReader(new InputStreamReader(openFileInput(fileName)));
-            String line;
+    class SaveTask extends AsyncTask<Student, Void, Long> {
 
-            while ((line = reader.readLine()) != null) {
-                builder.append(line);
+        private ProgressDialog mDialog;
+
+        @Override
+        protected void onPreExecute() {
+            super.onPreExecute();
+
+            mDialog = new ProgressDialog(MainActivity.this);
+            mDialog.setMessage("Wait...");
+            mDialog.setCancelable(false);
+            mDialog.show();
+        }
+
+        @Override
+        protected Long doInBackground(Student... params) {
+            long id = 0;
+
+            try {
+                Student student = params[0];
+
+                DataBaseHelper helper = new DataBaseHelper(MainActivity.this);
+                id = helper.insertStudent(student);
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
+            return id;
+        }
+
+        @Override
+        protected void onPostExecute(Long result) {
+            super.onPostExecute(result);
+
+            if (mDialog != null && mDialog.isShowing()) {
+                mDialog.dismiss();
             }
 
-            reader.close();
-
-            return builder.toString();
-        } catch (Exception e) {
-            e.printStackTrace();
-        }
-
-        return null;
-    }
-
-    private void saveExternalFile(File folder, String fileName, String data) {
-        try {
-            if (!folder.exists()) {
-                folder.mkdirs();
-            }
-
-            File file = new File(folder, fileName);
-
-            BufferedWriter writer = new BufferedWriter(new OutputStreamWriter(new FileOutputStream(file), "UTF8"));
-
-            writer.write(data);
-            writer.flush();
-            writer.close();
-        } catch (Exception e) {
-            e.printStackTrace();
+            Toast.makeText(MainActivity.this, "id:" + result, Toast.LENGTH_SHORT).show();
         }
     }
 
-    private String readExternalFile(File folder, String fileName) {
-        File file = new File(folder, fileName);
+    class GetTask extends AsyncTask<Long, Void, Student> {
 
-        try {
-            if (file.exists()) {
-                StringBuilder builder = new StringBuilder();
-                BufferedReader reader = new BufferedReader(new FileReader(file));
-                String line;
+        private ProgressDialog mDialog;
 
-                while ((line = reader.readLine()) != null) {
-                    builder.append(line);
-                }
+        @Override
+        protected void onPreExecute() {
+            super.onPreExecute();
 
-                reader.close();
-
-                return builder.toString();
-            }
-        } catch (Exception e) {
-            e.printStackTrace();
+            mDialog = new ProgressDialog(MainActivity.this);
+            mDialog.setMessage("Wait...");
+            mDialog.setCancelable(false);
+            mDialog.show();
         }
 
-        return null;
+        @Override
+        protected Student doInBackground(Long... params) {
+            Student student = null;
+
+            try {
+                long id = params[0];
+
+                DataBaseHelper helper = new DataBaseHelper(MainActivity.this);
+                student = helper.getStudent(id);
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
+            return student;
+        }
+
+        @Override
+        protected void onPostExecute(Student result) {
+            super.onPostExecute(result);
+
+            if (mDialog != null && mDialog.isShowing()) {
+                mDialog.dismiss();
+            }
+
+            if (result == null) {
+                Toast.makeText(MainActivity.this, "Student not found", Toast.LENGTH_SHORT).show();
+            } else {
+                Toast.makeText(MainActivity.this, result.FirstName, Toast.LENGTH_SHORT).show();
+            }
+        }
     }
 }
